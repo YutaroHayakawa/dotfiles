33b0c4b3f4896985472022b4d9dd889b613fc8d0
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2015-04-15 16:40:41 +0900

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#NeoBundleのための環境構築
+if [-e ~/.vim/bundle]; then
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+else
+    echo "~/.vim/bundle doesn't exist! I will make it!"
+    mkdir ~/.vim/bundle
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
+
+#カラースキームのための環境構築
+if [-e ~/.vim/colors]; then
+    cd ~/.vim/colors
+    git clone https://github.com/tomasr/molokai.git
+    cp molokai/colors/molokai.vim .
+else
+    echo "~/.vim/colors doesn't exist! I will make it!"
+    mkdir ~/.vim/colors
+    cd ~/.vim/colors
+    git clone https://github.com/tomasr/molokai.git
+    cp molokai/colors/molokai.vim .
+fi

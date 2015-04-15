89e199f68af54e9ddf71a121ade9629ab148b301
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2015-04-15 20:40:05 +0900

    Some change to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,32 +3,20 @@ cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
 
 #NeoBundleのための環境構築
-if [-e ~/.vim/bundle]; then
-    mkdir -p ~/.vim/bundle
-fi
+mkdir -p ~/.vim/bundle
 
-if [-e ~/.vim/colors] then
-    mkdir -p ~/.vim/colors
-fi
+mkdir -p ~/.vim/colors
 
-if [-e ~/.vim/bundle/neobundle]; then
-    cd ~/.vim/bundle
-    git clone https://github.com/Shougo/neobundle.vim
-fi
+cd ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim
 
 #vimカラースキームのための環境構築
-if [-e ~/.vim/colors/molokai]; then
-    cd ~/.vim/colors
-    git clone https://github.com/tomasr/molokai.git
-    cp molokai/colors/molokai.vim .
-fi
+cd ~/.vim/colors
+git clone https://github.com/tomasr/molokai.git
+cp molokai/colors/molokai.vim .
 
 #zshrcのための環境構築
-if [-e ~/.zshrc]; then
-    git clone https://github.com/seebi/dircolors-solarized.git ~/dircolors-solarized &&
-    ln -s ~/dircolors-solarized/dircolors.256dark ~/.dircolors &&
-    source ~/.zshrc
-else
-    echo "~/.zshrc doesn't exists!"
-fi
+git clone https://github.com/seebi/dircolors-solarized.git ~/dircolors-solarized &&
+ln -s ~/dircolors-solarized/dircolors.256dark ~/.dircolors &&
+source ~/.zshrc
 

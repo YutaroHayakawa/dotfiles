661ca2f36ba7fc619c274abcb6cab8d22c85853c
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2017-07-02 11:51:56 +0000

    Updated

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ mkdir -p ~/.vim/bundle
 
 mkdir -p ~/.vim/colors
 
+git config --global user.email "river@ht.sfc.keio.ac.jp"
+git config --global user.name "Yutaro Hayakawa"
+
 cd ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim
 
@@ -16,6 +19,6 @@ git clone https://github.com/tomasr/molokai.git
 cp molokai/colors/molokai.vim .
 
 #zshrcのための環境構築
-git clone https://github.com/seebi/dircolors-solarized.git ~/dircolors-solarized &&
-ln -s ~/dircolors-solarized/dircolors.256dark ~/.dircolors &&
+git clone https://github.com/seebi/dircolors-solarized.git ~/.dircolors-solarized &&
+ln -s ~/.dircolors-solarized/dircolors.256dark ~/.dircolors &&
 source ~/.zshrc

8ee1148a49800671955d02023cce3f6fb8d6f2aa
Yutaro Hayakawa <yutaro.hayakawa@linecorp.com>
2020-08-16 22:59:47 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,37 @@
-#このレポジトリのファイルをシンボリックリンクにする
+#!/bin/sh
+
+# Create symlinks for rc files
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/zshrc ~/.zshrc
 
-#NeoBundleのための環境構築
-mkdir -p ~/.vim/bundle
-
-mkdir -p ~/.vim/colors
-
-git config --global user.email "river@ht.sfc.keio.ac.jp"
+# Default git config, should overwrite in each repo
+git config --global user.email "yhayakawa3720@gmail.com"
 git config --global user.name "Yutaro Hayakawa"
 
-cd ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim
+# Create required directories for vim
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/colors
 
-#vimカラースキームのための環境構築
+# Install vim color scheme
 cd ~/.vim/colors
-git clone https://github.com/tomasr/molokai.git
-cp molokai/colors/molokai.vim .
+git clone https://github.com/phanviet/vim-monokai-pro
+cp vim-monokai-pro/colors/monokai_pro.vim .
 
-#zshrcのための環境構築
+# Install zsh color scheme
 git clone https://github.com/seebi/dircolors-solarized.git ~/.dircolors-solarized &&
 ln -s ~/.dircolors-solarized/dircolors.256dark ~/.dircolors &&
 source ~/.zshrc
+
+# Change the default shell
+kernel=`uname -s`
+if [ $kernel = "Darwin" ]; then
+  chsh -s `which zsh`
+elif [ $kernel = "Linux" ]; then
+  sudo uname -s `which zsh` `whoami`
+else
+  echo "Unknown kernel name $kernel"
+  exit 1
+fi
+
+# Install pyenv required for vim plugins
+curl https://pyenv.run | sh
